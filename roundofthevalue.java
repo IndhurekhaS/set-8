7ad799c235b1a1914a0dcc260db2951d8d04c9fd
IndhurekhaS <42089681+IndhurekhaS@users.noreply.github.com>
2019-05-08 12:37:51 +0530

    Create roundofthevalue.java

---
+++ roundofthevalue.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class decimal{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double s1=sc.nextDouble();
+        double a=Math.ceil(s1);
+        System.out.println(Math.round(a));
+    }
+}
